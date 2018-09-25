8e77d4f7c2cfbf0195cc36d112fa2bba6691724f
Bob Hamblok <post@bhamblok.be>
2018-09-25 15:30:35 +0200

    reload new date

--- js/app.js
+++ js/app.js
@@ -7,23 +7,35 @@ const query = window.location.search.replace('?', '').split('&').reduce((prev, n
 }, {});
 const DAYSOFWEEK = ['none', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'doktransporten'];
 let TODAY = Math.round(new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()).getTime()/1000);
-if (query.date) {
-  TODAY = Math.round(new Date(query.date.split('-')[0], query.date.split('-')[1] - 1, query.date.split('-')[2]).getTime()/1000);
-}
-const startDate = TODAY - (60 * 60 * 24) * 1;
-const endDate = TODAY + (60 * 60 * 24) * 6;
+// if (query.date) {
+//   TODAY = Math.round(new Date(query.date.split('-')[0], query.date.split('-')[1] - 1, query.date.split('-')[2]).getTime()/1000);
+// }
+// const startDate = TODAY - (60 * 60 * 24) * 1;
+// const endDate = TODAY + (60 * 60 * 24) * 6;
 const DAY = DAYSOFWEEK[query.day || 1];
 const DOKTRANSPORTEN = query.day === 6;
 let XMLString = '';
 
-let url = `http://192.168.16.15:8980/REST_PLAN_TCAN/rest/REST_PLAN_TCANService/api/v1/requestplanning?from=${startDate}&till=${endDate}`;
-if (query.date && location.hostname === 'localhost') {
-  url = '/api/v1/data/test.xml';
-  autoReload = '';
-}
+// let url = `http://192.168.16.15:8980/REST_PLAN_TCAN/rest/REST_PLAN_TCANService/api/v1/requestplanning?from=${startDate}&till=${endDate}`;
+// if (query.date && location.hostname === 'localhost') {
+//   url = '/api/v1/data/test.xml';
+//   autoReload = '';
+// }
 
 // INIT
 function init() {
+  TODAY = Math.round(new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()).getTime()/1000);
+  if (query.date) {
+    TODAY = Math.round(new Date(query.date.split('-')[0], query.date.split('-')[1] - 1, query.date.split('-')[2]).getTime()/1000);
+  }
+  const startDate = TODAY - (60 * 60 * 24) * 1;
+  const endDate = TODAY + (60 * 60 * 24) * 6;
+  let url = `http://192.168.16.15:8980/REST_PLAN_TCAN/rest/REST_PLAN_TCANService/api/v1/requestplanning?from=${startDate}&till=${endDate}`;
+  if (query.date && location.hostname === 'localhost') {
+    url = '/api/v1/data/test.xml';
+    autoReload = '';
+  }
+  
   // ADD HEADER TITLE
   const header = document.querySelector('tcl-header');
   header.removeAttribute('autoreload');
